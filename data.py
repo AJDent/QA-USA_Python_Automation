f97ef0026db40a033d89f58efbb591d4bc9537e3
ajdent <adarrylldentjr@gmail.com>
2025-02-17 20:08:42 -0500

    Sending to Github QA-USA

---
+++ data.py
@@ -0,0 +1,10 @@
+URBAN_ROUTES_URL = 'https://cnt-5489d76d-65fc-46f1-bb08-513fadbaa4b3.containerhub.tripleten-services.com'
+ADDRESS_FROM = 	'East 2nd Street, 601'
+ADDRESS_TO = 	'1300 1st St'
+PHONE_NUMBER = 	'+1 123 123 12 12'
+CARD_NUMBER = '1234 5678 9100'
+CARD_CODE = '1111'
+MESSAGE_FOR_DRIVER = 'Stop at the juice bar, please'
+
+def hello():
+    return "Hello from data.py!"
---
+++ main.py
@@ -0,0 +1,71 @@
+import data
+from helpers import is_url_reachable
+
+print(data.hello())  # Confirms data.py is working
+
+
+class TestUrbanRoutes:
+    @classmethod
+    def setup_class(cls):
+        # Add in S8
+        URBAN_ROUTES_URL = data.URBAN_ROUTES_URL
+
+        if is_url_reachable(URBAN_ROUTES_URL):
+           print("Connected to the Urban Routes server")
+        else:
+           print("Cannot connect to Urban Routes server")
+           pass
+
+    def test_set_route(self):
+        # Add in S8
+        print("Function created for set route")
+        pass
+
+    def test_select_plan(self):
+        # Add in S8
+        print("Function created for select plan")
+        pass
+
+    def test_fill_phone_number(self):
+        # Add in S8
+        print("Function created for fill phone number")
+        pass
+
+    def test_fill_card(self):
+        # Add in S8
+        print("Function created for fill card")
+        pass
+
+    def test_comment_for_driver(self):
+        # Add in S8
+        print("Function created for comment for driver")
+        pass
+
+    def test_order_blanket_and_handkerchiefs(self):
+        # Add in S8
+        print("Function created for order blanket and handkerchiefs")
+        pass
+
+    def test_order_2_ice_creams(self):
+        # Add in S8
+        for i in range(2):
+         print("Function created for order 2 ice creams")
+        pass
+
+    def test_car_search_model_appears(self):
+        # Add in S8
+        print("Function created for car search model appears")
+        pass
+
+
+# ✅ Ensure this code is OUTSIDE the class (after its definition)
+if __name__ == "__main__":
+    test = TestUrbanRoutes()  # Create an instance of the class
+    test.test_set_route()
+    test.test_select_plan()
+    test.test_fill_phone_number()
+    test.test_fill_card()
+    test.test_comment_for_driver()
+    test.test_order_blanket_and_handkerchiefs()
+    test.test_order_2_ice_creams()
+    test.test_car_search_model_appears()
